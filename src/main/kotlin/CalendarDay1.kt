6d91bf46ae39bcbb8b2770cece5e341889826385
Maëlise Castel <maelise.castel@flexicty.energy>
2024-12-01 21:10:06 +0100

    calendar day 1

---
+++ src/main/kotlin/CalendarDay1.kt
@@ -0,0 +1,24 @@
+package main.kotlin
+
+import com.google.gson.Gson
+import kotlin.math.abs
+
+fun calendarDay1() {
+    val jsonData = {}.javaClass.getResource("/calendar_day_1.json")!!.readText()
+    var gson = Gson()
+    val calendarDay1Input = gson.fromJson(jsonData, CalendarDay1Input::class.java)
+
+    val locationIds1 = calendarDay1Input.locationIds1.sorted()
+    val locationIds2 = calendarDay1Input.locationIds2.sorted()
+
+    var distance = 0
+    var similarityScore = 0
+    locationIds1.forEachIndexed { index, locationId1 ->
+        distance += abs(locationId1 - locationIds2[index])
+        similarityScore += locationIds2.count { it == locationId1 } * locationId1
+    }
+    println("Distance: $distance")
+    println("Similarity score: $similarityScore")
+}
+
+private data class CalendarDay1Input(val locationIds1: List<Int>, val locationIds2: List<Int>)
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package main.kotlin
+
+fun main() {
+    calendarDay1()
+}
